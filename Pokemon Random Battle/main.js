cdf361b708fbfb12926a50f93d5abe206a1dd509
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2024-09-06 21:42:49 -0500

    jsdocs like comments added

--- Pokemon Random Battle/main.js
+++ Pokemon Random Battle/main.js
@@ -11,25 +11,47 @@ const looserText = document.querySelector("#looser")
 
 let p1, p2,img1,img2,attack1,attack2;
 
+/**
+ * Retrieves a list of all Pokémon from the PokeAPI.
+ *
+ * @return {Promise} The response of the API in json.
+ */
 async function getRandomPokemon() {
   const pokemon = await fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0',{mode:'cors'})
   return pokemon.json()
 }
-// refactor con async await, esa vaina esta muy dura solo con promises
+
+/**
+ * Retrieves the image of a given Pokemon from the PokeAPI.
+ *
+ * @param {string} pokemon - The name of the Pokemon.
+ * @return {Promise} The response of the API in json.
+ */
 async function getPokemonImage(pokemon) {
   const pokemonImage = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
   return pokemonImage.json()
 }
 
+
+/**
+ * Gets two random Pokémon names from the Pokémon API.
+ *
+ * @return {void}
+ */
 async function gatherPokemons(){
   p1 = await getRandomPokemon()
   p1 = p1.results[Math.floor(Math.random() * 1302)].name
-  // p1 = p1[0].toUpperCase() + p1.substring(1)
   p2 = await getRandomPokemon()
   p2 = p2.results[Math.floor(Math.random() * 1302)].name
-  // p2 = p2[0].toUpperCase() + p2.substring(1)
 }
 
+/**
+ * Sets the images for two Pokémon based on their names.
+ *
+ * @param {string} pokemonName1 - The name of the first Pokémon.
+ * @param {string} pokemonName2 - The name of the second Pokémon.
+ * @return {void}
+ */
 async function setImages(pokemonName1, pokemonName2){
   img1 = await getPokemonImage(pokemonName1)
   img1 = img1.sprites.other['official-artwork']['front_default']
@@ -39,11 +61,25 @@ async function setImages(pokemonName1, pokemonName2){
 
 
 // game logic,the pokemon Id will be used as the attack points
+/**
+ * Retrieves the ID of a given Pokemon from the PokeAPI.
+ *
+ * @param {string} pokemon - The name of the Pokemon.
+ * @return {Promise} The response of the API in json.
+ */
 async function getPokemonId(pokemon){
   const pokemonId = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
   return pokemonId.json()
 }
 
+
+/**
+ * Sets the battle points for two Pokemon based on their IDs.
+ *
+ * @param {string} pokemon1 - The name of the first Pokemon.
+ * @param {string} pokemon2 - The name of the second Pokemon.
+ * @return {void}
+ */
 async function setPokemonPoints(pokemon1, pokemon2){
   attack1 = await getPokemonId(pokemon1)
   attack1 = attack1.id
@@ -51,6 +87,11 @@ async function setPokemonPoints(pokemon1, pokemon2){
   attack2 = attack2.id
 }
 
+/**
+ * Plays a match between two Pokemon.
+ *
+ * @return {void}
+ */
 function playMatch(){
   if(resultsDiv.classList.contains("invisible")){
     resultsDiv.classList.remove("invisible")
@@ -64,7 +105,12 @@ function playMatch(){
   }
 }
 
-// functions used to paint the sprites
+
+/**
+ * Paints the names and images of the two pokemons on the screen
+ * @function
+ * @param {void} - No parameters
+ */
 function drawPokemons(){
   containers.forEach(div=>{
     // renders the names
@@ -84,16 +130,36 @@ function drawPokemons(){
     }
   })
 }
+
+
+
+/**
+ * Function that listens for clicks on the button and triggers the game
+ * @function
+ * @param {Event} e - The event triggered by the button
+ */
 btn.addEventListener('click', async (e) => {
-  // resets the winner
+  /**
+   * Resets the results div to invisible
+   */
   if(!resultsDiv.classList.contains("invisible")){
     resultsDiv.classList.add("invisible")
   }
-  // calls the API to get the name
+
+  /**
+   * Calls the API to get the name and image of the pokemon
+   */
   await gatherPokemons()
   await setImages(p1,p2)
   await setPokemonPoints(p1,p2)
 
+  /**
+   * Paints the sprites
+   */
   drawPokemons()
+  /**
+   * Triggers the game logic after 1 second
+   */
   setTimeout(playMatch,1000)
 })
+

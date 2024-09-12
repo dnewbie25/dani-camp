c6fecee1c8117914ec05c2fdc563636569a63f65
dfrojasley <dfrojas.feld@gmail.com>
2024-09-12 15:47:00 -0500

    complete basic feature implementation

--- Pokemon Random Battle/main.js
+++ Pokemon Random Battle/main.js
@@ -3,13 +3,57 @@
 const pokemon1 = document.getElementById("pokemon1");
 const pokemon2 = document.getElementById("pokemon2");
 const btn = document.querySelector(".btn");
-const containers = document.querySelectorAll(".pokemon")
-const resultsDiv = document.querySelector(".match-results")
-const winnerText = document.querySelector("#winner")
-const looserText = document.querySelector("#looser")
+const containers = document.querySelectorAll(".pokemon");
+const resultsDiv = document.querySelector(".match-results");
+const winnerText = document.querySelector("#winner");
+const looserText = document.querySelector("#looser");
+const pokemon1DataContainer = document.getElementById('pokemon1DataContainer');
+const pokemon2DataContainer = document.getElementById('pokemon2DataContainer');
+const pokemon1DataType = document.getElementById('pokemon1DataType');
+const pokemon2DataType = document.getElementById('pokemon2DataType');
+const pokemon1DataAttack = document.getElementById('pokemon1DataAttack');
+const pokemon2DataAttack = document.getElementById('pokemon2DataAttack');
+const pokemon1DataDefense = document.getElementById('pokemon1DataDefense');
+const pokemon2DataDefense = document.getElementById('pokemon2DataDefense');
 // these means pokemon 1 and 2, images 1 and 2 that corresponds to each pokemon. The attack 1 and 2 represents the battle points (the id will be the attack points for each pokemon for now)
 
 let p1, p2,img1,img2,attack1,attack2;
+let type1,type2,realAttack1,realAttack2, defence1,defence2;
+
+window.addEventListener( 'onload', hideStats())
+/**
+ * hides pokemon stats on window load
+ */
+function hideStats() {
+  pokemon1DataContainer.style.display = 'none'
+  pokemon2DataContainer.style.display = 'none'
+};
+
+/**
+ *  Unhides all stats after they have finished loading
+ */
+function showStats() {
+  //invoke and set the pokemon type
+  setType( p1 , p2 );
+  //invoke and set the pokemon attack
+  setAttack( p1 , p2 );
+  //invoke and set the pokemon defence
+  setDefence( p1 , p2 );
+  //unhide the stats
+  setTimeout(() => {
+    //add the data to the stats section
+    pokemon1DataType.innerText = type1;
+    pokemon2DataType.innerText = type2;
+    pokemon1DataAttack.innerText = realAttack1;
+    pokemon2DataAttack.innerText = realAttack2;
+    pokemon1DataDefense.innerText = defence1;
+    pokemon2DataDefense.innerText = defence2;
+    //unhide all the stats
+    pokemon1DataContainer.style.display = 'block';
+    pokemon2DataContainer.style.display = 'block';
+  }, 500);
+
+};
 
 /**
  * Retrieves a list of all Pokémon from the PokeAPI.
@@ -32,6 +76,10 @@ async function getPokemonImage(pokemon) {
   return pokemonImage.json()
 }
 
+async function getPokemonStats(pokemonName) {
+  const pokemonType = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+  return pokemonType.json()
+}
 
 /**
  * Gets two random Pokémon names from the Pokémon API.
@@ -53,12 +101,47 @@ async function gatherPokemons(){
  * @return {void}
  */
 async function setImages(pokemonName1, pokemonName2){
-  img1 = await getPokemonImage(pokemonName1)
+  img1 = await getPokemonImage(pokemonName1);
   img1 = img1.sprites.other['official-artwork']['front_default']
   img2 = await getPokemonImage(pokemonName2)
   img2 = img2.sprites.other['official-artwork']['front_default']
 }
 
+/**
+ * get the pokemon type
+ * @param {string} pokemonName1 - 1st pokemon name
+ * @param {string}} pokemonName2 - 2nd pokemon name
+ */
+async function setType(pokemonName1, pokemonName2) {
+  type1 = await getPokemonStats(pokemonName1);
+  type1 = type1.types[0].type.name;
+  type2 = await getPokemonStats(pokemonName2);
+  type2 = type2.types[0].type.name;
+};
+
+/**
+ * get the pokemon attack stat
+ * @param {string} pokemonName1 - 1st pokemon name
+ * @param {string}} pokemonName2 - 2nd pokemon name
+ */
+async function setAttack(pokemonName1, pokemonName2) {
+  realAttack1 = await getPokemonStats(pokemonName1);
+  realAttack1 = realAttack1.stats[1].base_stat;
+  realAttack2 = await getPokemonStats(pokemonName2);
+  realAttack2 = realAttack2.stats[1].base_stat;
+};
+
+/**
+ * get the pokemon defence stat
+ * @param {string} pokemonName1 - 1st pokemon name
+ * @param {string}} pokemonName2 - 2nd pokemon name
+ */
+async function setDefence(pokemonName1, pokemonName2) {
+  defence1 = await getPokemonStats(pokemonName1);
+  defence1 = defence1.stats[2].base_stat;
+  defence2 = await getPokemonStats(pokemonName2);
+  defence2 = defence2.stats[2].base_stat;
+};
 
 // game logic,the pokemon Id will be used as the attack points
 /**
@@ -150,8 +233,9 @@ btn.addEventListener('click', async (e) => {
    * Calls the API to get the name and image of the pokemon
    */
   await gatherPokemons()
-  await setImages(p1,p2)
-  await setPokemonPoints(p1,p2)
+  await setImages( p1 , p2 );
+  await setPokemonPoints( p1 , p2 );
+  await showStats( p1 , p2 );
 
   /**
    * Paints the sprites

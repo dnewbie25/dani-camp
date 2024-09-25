c456c8a75b1712f2a87087277d47e75e54f3c984
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2024-09-25 11:22:27 -0500

    Refactor API Verbs App code for improved functionality and readability

--- api-verbs-app/README.md
+++ api-verbs-app/README.md
@@ -10,9 +10,13 @@ This is a basic JS and HTML app to test API verbs like PUT, POST, GET and DELETE
 ## Endpoints
 
 * GET: https://api-verbs-app.onrender.com/get
+  * Accepts an integer as id (default books go from `1` to `6`), or the string `all` to retrieve all books
 * POST: https://api-verbs-app.onrender.com/post
+  * Accepts a string with the id, title, author and category, separated by commas, i.e, `7,Harry Potter and the Hollows, J Rowling,Fantasy`
 * PUT: https://api-verbs-app.onrender.com/put
+  * Accepts a string with the id to modify, along with title, author and category, separated by commas, i.e, `7,Harry Potter and the Deadly Hollows, J.K. Rowling,Fantasy`
 * DELETE: https://api-verbs-app.onrender.com/delete
+  * Accepts a string with an id or ids of the books to delete: `1` or `1,6,2`
 
 ## Technologies Used
 
--- api-verbs-app/main.js
+++ api-verbs-app/main.js
@@ -1,36 +1,71 @@
 const get = document.getElementById('get');
 const post = document.getElementById('post');
 const put = document.getElementById('put');
-const deleteBtn = document.getElementById('delete');
+const deleteField = document.getElementById('delete');
 const requestBtn = document.getElementById('btn');
 const getP = document.getElementById('getP');
 const postP = document.getElementById('postP');
 const putP = document.getElementById('putP');
 const deleteP = document.getElementById('deleteP');
 
+/**
+ * Handles the click event of the request button
+ * @param {Event} e - The event object
+ * @return {void}
+ */
 requestBtn.addEventListener('click', async (e) => {
-  const dataGet = get.value.split(',')
-  const dataPost = post.value.split(',')
-  const getResult = await getFunction(dataGet)  
-  const postResult = await postFunction(dataPost)  
-  getP.textContent = JSON.stringify(getResult['args'])
-  postP.textContent = JSON.stringify(postResult['args'])
+  e.preventDefault();
+  if(get.value){
+    const getResult = await getFunction(get.value)  
+    getP.textContent = JSON.stringify(getResult)
+  }
+  if(post.value){
+    await postFunction(post.value)  
+  }
+  if(put.value){
+    const putResult = await putFunction(put.value)  
+    console.log(putResult);
+  }
+  if(deleteField.value){
+    const deleteResult = await deleteFunction(deleteField.value)
+    console.log(deleteResult);
+  }
+  get.value = ''
+  post.value = ''
+  put.value = ''
+  deleteField.value = ''
 })
 
 
+/**
+ * Sends an HTTP GET request to the specified URL and returns the response data in JSON format.
+ *
+ * @param {string} data - The data to be appended to the URL for the GET request (id or 'all')
+ * @return {object} The response data from the server in JSON format
+ */
 async function getFunction(data) {
   if (!get.value) {
     throw new Error('No value has been passed for an HTTP GET request. Please input a value')
   }
   try{
-    const url = await fetch(`https://httpbin.org/get?name=${data[0]}&age=${data[1]}&class=${data[2]||Math.floor(1000 + Math.random() * 9000)}`,
-      { mode: 'cors'})
+    const url = await fetch(`http://127.0.0.1:8000/get/${data}`,
+      {
+        mode:'cors'
+      })
     return url.json()
+    
   }catch{
     throw new Error('HTTP Get error, please check your input')
   }
 }
 
+/**
+ * Sends an HTTP POST request to the specified URL with the given data.
+ *
+ * @param {string} data - The data to be sent in the request body.
+ * @throws {Error} If no value has been passed for the POST request.
+ * @throws {Error} If there is an error with the HTTP POST request.
+ */
 async function postFunction(data) {
   if (!post.value) {
     throw new Error('No value has been passed for an HTTP POST request. Please input a value')
@@ -39,14 +74,64 @@ async function postFunction(data) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
+    }
   };
   try{
-    const url = await fetch(`https://httpbin.org/post?name=${data[0]}&age=${data[2]||Math.floor(1000 + Math.random() * 9000)}`,
-      options)
-    return url.json()
+    await fetch(`http://127.0.0.1:8000/post/${data}`,
+      options)    
   }catch{
     throw new Error('HTTP Post error, please check your input')
+  }
+}
+
+/**
+ * Sends an HTTP PUT request to the specified URL with the given data.
+ *
+ * @param {string} data - The data to be sent in the request body.
+ * @throws {Error} If no value has been passed for the PUT request.
+ * @throws {Error} If there is an error with the HTTP PUT request.
+ */
+async function putFunction(data) {
+  if (!put.value) {
+    throw new Error('No value has been passed for an HTTP PUT request. Please input a value')
+  }
+  const options = {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  };
+  try{
+    const url = await fetch(`http://127.0.0.1:8000/put/${data}`,
+      options)
+    console.log(url);
+    
+  }catch{
+    throw new Error('HTTP Put error, please check your input')
+  }
+}
+
+/**
+ * Sends an HTTP DELETE request to the specified URL with the given data.
+ *
+ * @param {string} data - The data to be sent in the request body.
+ * @throws {Error} If no value has been passed for the DELETE request.
+ * @throws {Error} If there is an error with the HTTP DELETE request.
+ */
+async function deleteFunction(data) {
+  if(!deleteField.value){
+    throw new Error('No value has been passed for an HTTP DELETE request. Please input a value')
+  }
+  const options = {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+  try{
+    await fetch(`http://127.0.0.1:8000/delete/?books=${data}`,
+      options)
+  }catch{
+    throw new Error('HTTP Delete error, please check your input')
   }
 }

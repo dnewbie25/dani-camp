042bd05c555bfba505c536f9e6959adadda5a4c0
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2024-11-26 21:38:22 -0500

    react basics completed

---
+++ react-basics/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+const config = () => {
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js'
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react'],
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  }
+}
+
+module.exports = config

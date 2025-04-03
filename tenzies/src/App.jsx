29693444d1dabdf787950439b710c9132594ff94
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2025-04-02 23:19:51 -0500

    confetti and game won conditions added

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from "react";
 import "./App.css";
 import Die from "./Die";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti"
 
 function App() {
   const [numbers, setNumbers] = useState(generateAllNewDice());
+  let gameWon = false
+
+  if (
+    numbers.every((item) => item.isHeld === true) &&
+    numbers.every((item) => item.value === numbers[0].value)
+  ) {
+    gameWon = true
+  }
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => ({
       value: Math.ceil(Math.random() * 6),
@@ -41,11 +51,15 @@ function App() {
 
   return (
     <main>
+      {gameWon && <Confetti/>}
       <h1 className="title">Tenzies</h1>
-      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
       <div className="dice-container">{dices}</div>
       <button onClick={rollDice} className="roll-dice">
-        Roll
+        { gameWon ? "New Game":"Roll"}
       </button>
     </main>
   );

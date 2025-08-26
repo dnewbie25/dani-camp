fcb064ea3bb4f4da5708356e338837177741f601
Daniel Montoya <38549672+dnewbie25@users.noreply.github.com>
2025-08-25 22:11:59 -0500

    updated the main page with instructions

--- ai-chef/src/Main_app.jsx
+++ ai-chef/src/Main_app.jsx
@@ -30,6 +30,24 @@ export default function Main_app() {
 
   return (
     <main>
+      <section>
+        <h2>How to use?</h2>
+        <ol>
+          <li>
+            Enter the ingredients one by one by hitting Enter or pressing the "+
+            Add Ingredient"
+          </li>
+          <li>
+            Once you have 4 or more ingredients a "Get Recipe" button will show
+            up
+          </li>
+          <li>
+            Press that button to get your own custom recipe based on the
+            ingredients
+          </li>
+          <li>Put your cooking skills into practice with your new recipe!</li>
+        </ol>
+      </section>
       <form className="add-ingredient-form" action={handleSubmit}>
         <input
           type="text"
